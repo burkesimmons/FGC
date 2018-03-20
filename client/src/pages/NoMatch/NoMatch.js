830bc9c48997389d170be1c1c6d5470e22ac77a0
burkesimmons <burkesimmons@gmail.com>
2018-03-19 23:25:00 -0600

    don't know, can't remember

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,21 +1,9 @@
 import React from "react";
-import { Col, Row, Container } from "../../components/Grid";
-import { Jumbotron } from "reactstrap";
+import {Container} from 'reactstrap';
 
 const NoMatch = () =>
   <Container fluid>
-    <Row>
-      <Col size="md-12">
-        <Jumbotron>
-          <h1>404 Page Not Found</h1>
-          <h1>
-            <span role="img" aria-label="Face With Rolling Eyes Emoji">
-              🙄
-            </span>
-          </h1>
-        </Jumbotron>
-      </Col>
-    </Row>
+    <h1>NOmatch</h1>
   </Container>;
 
 export default NoMatch;
